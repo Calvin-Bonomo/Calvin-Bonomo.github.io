a7232c07c598d0ef2ed11a4bb39f5d8105fbcf16
Calvin Bonomo <cal.bonomo@gmail.com>
2026-02-16 21:27:03 -0700

    Removed default next assets

---
+++ components/NavBar.tsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+    return (
+        <header className={`absolute top w-full shadow-sm flex`}>
+                <h1>Calvin Bonomo</h1>
+        </header>
+    );
+}
