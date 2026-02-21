19b4a63521c336738d4397a3e12b72f33966e89b
Calvin Bonomo <cal.bonomo@gmail.com>
2026-02-20 22:03:15 -0700

    Added fireside.png asset and formatted for small and medium screens

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,19 @@ const projects: ProjectInfo[] = projectData.projects as ProjectInfo[]
 export default function Home() {
   return (
       <div>
-        <div className={`grid grid-cols-2 gap-4 h-fit items-center mx-4 md:mx-0 lg:h-screen`}> {/*Intro*/}
-            <div className={`flex justify-end`}>
-                <div className={`md:w-2/3 lg:w-1/2`}>
-                    <h1 className={`text-4xl lg:text-6xl font-bold overflow-visible`}>Hi, I'm Calvin</h1>
-                    <p className={`text-sm md:text-lg`}>I'm a fourth year undergraduate student at the University of Colorado Boulder, pursuing a Bachelor of Science in Computer Science at the University of Colorado Boulder. I previously interned at Apple on the visionOS UI and Frameworks team where I did prototyping and exploratory work as well as learning about API development.</p>
-                </div>
-            </div>
-            <Image className={`sm:m-1 md:w-2/3 lg:w-1/3 rounded-2xl`}
-            src="/Profile.jpg"
-            width={599}
-            height={599}
-            alt="Picture of Calvin Bonomo"
+        <div className="p-7 grid gap-x-5 md:grid-cols-2 md:p-25">
+            <h1 className="font-bold text-4xl text-center pb-4 md:text-5xl md:pb-0 md:text-start">Hi, I'm Calvin!</h1>
+            <div className="flex justify-start text-center md:row-span-2 md:w-full">
+            <Image className="rounded-xl md:w-3xs"
+                src="/Profile.jpg"
+                width={599}
+                height={599}
+                alt="Headshot of Calvin Bonomo"
             />
+            </div>
+            <p className="pt-4 md:pt-0">I'm a fourth year undergraduate student at the University of Colorado Boulder, pursuing a Bachelor of Science in Computer Science at the University of Colorado Boulder. I previously interned at Apple on the visionOS UI and Frameworks team where I did prototyping and exploratory work as well as learning about API development.</p>
         </div>
-        <div className="grid grid-cols-3 gaps-10 place-items-center px-10 my-10 inset-shadow-[0px_8px_5px_rgba(0,0,0,0.1),0px_-8px_5px_rgba(0,0,0,0.1)] max-h-2/3"> {/*Projects*/}
+        <div className="grid grid-rows-3 md:grid-rows-1 md:grid-cols-3 gap-10 p-10 place-items-center inset-shadow-[0px_8px_5px_rgba(0,0,0,0.1),0px_-8px_5px_rgba(0,0,0,0.1)]"> {/*Projects*/}
             {projects.map((project) => (
                 <ProjectCard key={project.id} id={project.id} info={project.info}/>
             ))}
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -15,16 +15,18 @@ export interface ProjectInfo {
 };
 
 const ProjectCard: React.FC<ProjectInfo> = ({ info }: ProjectInfo) => { return (
-    <div className="border-solid border-2 rounded-xl text-center my-10 p-5">
-        <h1 className="font-bold text-xl pb-4">{info.name}</h1>
-        <Image className="flex-grow pb-2"
+    <div className="border-solid border-2 rounded-xl text-center my-10 p-5 h-full w-full grid transition duration-500 hover:shadow-2xl">
+        <h1 className="font-bold text-xl">{info.name}</h1>
+        <Image className="flex-grow"
             src={info.image_path}
             alt="Placeholder"
             width={info.image_width}
             height={info.image_height}
         />
-        <p className="text-justify text-sm">{info.description}</p>
-        <Link className="underline" href={info.link}>Learn more</Link>
+        <p className="text-sm">{info.description}</p>
+        <div className="content-end">   
+            <Link className="p-2 rounded-md shadow inset-shadow transition ease-out duration-400 inset-shadow-[0px_0px_3px_rgba(0,0,0,0.2)] hover:inset-shadow-transparent not-hover:shadow-transparent hover:shadow-[0px_0px_10px_rgba(0,0,0,0.2)]" href={info.link}>Learn more</Link>
+        </div>
     </div>
 )};
 
